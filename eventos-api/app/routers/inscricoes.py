e7f81e6c33ed580bafc0f911992fdc21f52a2c89
Henrique K <henriqueandrekonrad@gmail.com>
2025-10-31 18:46:43 -0300

    inscricao_rapida autalizada para criar um usuario_rapido

--- eventos-api/app/routers/inscricoes.py
+++ eventos-api/app/routers/inscricoes.py
@@ -5,10 +5,17 @@ from app.models.inscricao import Inscricao
 from app.models.evento import Evento
 from app.models.usuario import Usuario
 from app import schemas
-from uuid import UUID
+from uuid import UUID, uuid4
+from secrets import token_urlsafe
 import datetime
 
 router = APIRouter(prefix="/inscricoes", tags=["Inscricoes"])
+
+def to_uuid(id_str: str):
+    try:
+        return UUID(id_str)
+    except Exception:
+        raise HTTPException(status_code=400, detail="ID inválido")
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def criar_inscricao_normal(evento_id: UUID, usuario_id: UUID, db: Session = Depends(get_db)):
@@ -52,20 +59,37 @@ def criar_inscricao_rapida(payload: schemas.InscricaoCreateRapida, db: Session =
     if not evento:
         raise HTTPException(status_code=404, detail="Evento não encontrado")
 
+    email = payload.email_rapido or f"temp_{uuid4()}@rapido.local"
+    usuario_rapido = Usuario(
+        nome=payload.nome_rapido,
+        email=email,
+        cpf=payload.cpf_rapido,
+        senha_hash=token_urlsafe(16),  # senha temporária
+        papel="rapido",
+        email_verificado=False
+    )
+    db.add(usuario_rapido)
+    db.commit()
+    db.refresh(usuario_rapido)
+
     inscr = Inscricao(
         evento_id=payload.evento_id,
         inscricao_rapida=True,
         nome_rapido=payload.nome_rapido,
         cpf_rapido=payload.cpf_rapido,
         email_rapido=payload.email_rapido,
-        status="ativa",
+        status="atvia",
         sincronizado=False
     )
     db.add(inscr)
     db.commit()
     db.refresh(inscr)
 
-    return {"id": str(inscr.id), "message": "inscrição rápida criada"}
+    return {
+        "inscricao_id": str(inscr.id),
+        "usuario_id": str(usuario_rapido.id),
+        "message": "Inscrição rápida criada com usuário rápido associado"
+    }
 
 @router.get("/evento/{evento_id}", response_model=list[schemas.InscricaoOut])
 def listar_inscricoes_por_evento(evento_id: UUID, db: Session = Depends(get_db)):
