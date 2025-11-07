5cc04a7caf2c5b9c486836943d7fa6ea3b700ffd
henriquekonrad <henrique.konrad@universo.univates.br>
2025-11-07 16:06:19 -0300

    teste authorization header

--- eventos-api/app/services/usuarios-service/main.py
+++ eventos-api/app/services/usuarios-service/main.py
@@ -3,7 +3,6 @@ Microsserviço de Usuários
 Porta: 8003
 """
 from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from uuid import UUID
 from passlib.context import CryptContext
@@ -13,6 +12,10 @@ from app.shared.models.usuario import Usuario
 from app.shared import schemas
 from app.shared.middlewares.add import add_common_middlewares
 from app.shared.core.security import require_roles
+from fastapi.openapi.utils import get_openapi
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+security = HTTPBearer()
 
 app = FastAPI(title="Usuarios Service", version="1.0.0")
 add_common_middlewares(app, audit=True)
