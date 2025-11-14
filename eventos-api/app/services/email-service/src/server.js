54307507bfc43080ad79eba40cee3f52ba95b9f0
henriquekonrad <henrique.konrad@universo.univates.br>
2025-11-14 14:15:10 -0300

    adicionado server.js

--- eventos-api/app/services/email-service/src/server.js
+++ eventos-api/app/services/email-service/src/server.js
@@ -1,1 +1,15 @@
-// inserir
+import express from "express";
+import cors from "cors";
+import emailRoutes from "./routes/email.js";
+import { config } from "./config.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/email", emailRoutes);
+
+app.listen(config.port, () => {
+  console.log(`Email service iniciado na porta ${config.port}`);
+});
