07c7b11ddd2eda51aa2b8283cd5ab30f4df6afe0
Henrique K <henriqueandrekonrad@gmail.com>
2025-11-25 19:28:58 -0300

    retirado icone de local

--- eventos-frontend/src/app/eventos-publicos/page.tsx
+++ eventos-frontend/src/app/eventos-publicos/page.tsx
@@ -116,27 +116,7 @@ export default async function EventosPublicosPage() {
                         </div>
                       </div>
 
-                      <div className="flex items-center">
-                        <svg
-                          className="h-5 w-5 mr-2 text-indigo-500"
-                          fill="none"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                          />
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                          />
-                        </svg>
-                      </div>
+                      
                     </div>
 
                     <Link
